595858eb5cf7b0e0c3db238d55d832cb0ac85aa9
YryRybkin <yurarybkin2@yandex.ru>
2022-11-13 01:18:20 +0300

    Create-drop ddl was the victim

--- filler.sql
+++ filler.sql
@@ -1,3 +1,5 @@
+\connect reservations
+
 CREATE TABLE reservation
 (
     id              SERIAL PRIMARY KEY,
@@ -23,6 +25,11 @@ CREATE TABLE hotels
     price     INT          NOT NULL
 );
 
+INSERT INTO hotels (id, hotel_uid, name, country, city, address, stars, price) VALUES
+(1, '049161bb-badd-4fa8-9d90-87c9a82b0668', 'Ararat Park Hyatt Moscow', 'Россия', 'Москва', 'Неглинная ул., 4', 5, 10000);
+
+\connect payments
+
 CREATE TABLE payment
 (
     id          SERIAL PRIMARY KEY,
@@ -31,6 +38,8 @@ CREATE TABLE payment
         CHECK (status IN ('PAID', 'CANCELED')),
     price       INT         NOT NULL
 );
+
+\connect loyalties
 
 CREATE TABLE loyalty
 (
@@ -42,8 +51,5 @@ CREATE TABLE loyalty
     discount          INT         NOT NULL
 );
 
-INSERT INTO hotels (id, hotel_uid, name, country, city, address, stars, price) VALUES
-(1, "049161bb-badd-4fa8-9d90-87c9a82b0668", "Ararat Park Hyatt Moscow", "Россия", "Москва", "Неглинная ул., 4", 5, 10000);
-
 INSERT INTO loyalty (id, username, reservation_count, status, discount)  VALUES
-(1, "Test Max", 25, "GOLD", 10);
+(1, 'Test Max', 25, 'GOLD', 10);
